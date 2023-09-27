ad5cd25b73bfd8cb24439d5973c79359e5a4d1a0
thejavidev <javidation.l@gmail.com>
2023-09-27 17:50:02 +0400

    v2

--- asstes/main.js
+++ asstes/main.js
@@ -2,13 +2,13 @@ let selectCounter = document.querySelector(".coutdown")
 
 
 const countToDate = new Date().setHours(new Date().getHours() + 192)
-
+let elapsedSeconds = 0;
 setInterval(() => {
     const currentDate = new Date()
     const timeBeetüenDates = Math.ceil((countToDate - currentDate) / 1000)
     flipAllCards(timeBeetüenDates, currentDate)
-    console.log(timeBeetüenDates)
 }, 1000);
+
 
 function pad(n) {
     return (n < 10 ? "0" + n : n)
@@ -16,6 +16,8 @@ function pad(n) {
 let previousDay = -1;
 
 function flipAllCards(time, currentDate) {
+
+
     const days = Math.ceil(time / (60 * 60 * 24))
     let seconds = time % 60
     let minutes = Math.floor(time / 60) % 60
@@ -40,15 +42,20 @@ function flip(flipCard, newNumber) {
     if (newNumber === startNumber) return
 
     const bottomHalf = flipCard.querySelector(".bottom");
+    bottomHalf.textContent = pad(startNumber);
 
     const topFlip = document.createElement("div");
     topFlip.classList.add("top-flip")
+    topFlip.textContent = pad(startNumber);
+
+
+
     const bottomFlip = document.createElement("div");
     bottomFlip.classList.add("bottom-flip");
-
-    bottomHalf.textContent = pad(startNumber);
-    topFlip.textContent = pad(startNumber);
     bottomFlip.textContent = pad(newNumber);
+   
+    
+    
 
     topFlip.addEventListener("animationstart", e => {
         topHalf.textContent = pad(newNumber);
